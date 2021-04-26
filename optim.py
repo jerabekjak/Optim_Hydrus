fe1250f9c44a4d86373c9e07dd9d842bd204eb3e
jerabekjak <jerabekjaku@gmail.com>
2021-04-26 11:33:52 +0200

    set params into SELECTOR.IN

--- optim.py
+++ optim.py
@@ -19,15 +19,46 @@ class Optim(object):
 
         return params
 
-    def set_params(self):
-        pass
+    def set_params(self, params):
+        """ Sets params in to SELECTOR.IN """
+
+        file_ = '{}/{}'.format(self.hp, 'SELECTOR.IN')
+        with open(file_,'r') as f:
+            lines = f.readlines()
+        NMat = int(lines[13].split()[0])
+
+        # TODO
+        #if not(len(params) == NMat): ERROR
+        
+        i = 0
+        for p in params: # replace lines in selector 
+            str_ = ' '.join([str(elem) for elem in p]) + '\n'
+            lines[26+i] = str_
+            i += 1
+
+        with open(file_,'w') as f:
+            f.writelines(lines)
+
+    def read_measured(self):
+        file_ = '{}/{}'.format(self.hp, 'SELECTOR.IN')
+        with open(file_,'r') as f:
+            lines = f.readlines()
+        NMat = int(lines[13].split()[0])
+
+
 
     def model(self, params):
         """ params ::   thr ths Alfa n Ks l
         in case of 2 soils paras list of two lists
         """
         cmd = './{} {}'.format(self.exe, self.hp)
-        os.system(cmd)
+
+        self.set_params(params)
+
+
+
+        #os.system(cmd)
 
     def run(self):
         initparams = self.get_params()
+        self.model(initparams)
