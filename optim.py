86b93c53b05a1bc4389e77813486b8f1eb173185
jerabekjak <jerabekjaku@gmail.com>
2021-04-26 10:58:17 +0200

    init model(params)funrcion

--- optim.py
+++ optim.py
@@ -1,6 +1,17 @@
+import os
+
 class Optim(object):
     
     def __init__(self,hydro_proj, outdir):
-        pass
+        self.hp = hydro_proj
+        self.outdir = outdir
+        self.exe = 'H1D_CALC.EXE'
+
+    def model(self,params):
+        cmd = './{} {}'.format(self.exe, self.hp)
+        os.system(cmd)
+        
+
+
     def run(self):
         pass
