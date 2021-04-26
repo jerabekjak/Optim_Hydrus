941352b563cd652db4bc5fa689325540ed12aef1
jerabekjak <jerabekjaku@gmail.com>
2021-04-26 12:01:00 +0200

    read measured data

--- optim.py
+++ optim.py
@@ -1,4 +1,16 @@
 import os
+import numpy as np
+
+class Data(object):
+    def __init__(self, time, val, position):
+        self._n = len(time)
+        self.time = np.zeros(self._n, float)
+        self.val = np.zeros(self._n, float)
+        self.positoin = np.zeros(self._n, float)
+
+        self.time = [float(i) for i in time]
+        self.val = [float(i) for i in val]
+        self.position = [int(i) for i in position]
 
 class Optim(object):
     
@@ -6,6 +18,9 @@ class Optim(object):
         self.hp = hydro_proj
         self.outdir = outdir
         self.exe = 'H1D_CALC.EXE'
+
+        tmp = self.read_measured()
+        self.obs = Data(tmp[0], tmp[1], tmp[2])
 
     def get_params(self):
         file_ = '{}/{}'.format(self.hp, 'SELECTOR.IN')
@@ -40,12 +55,28 @@ class Optim(object):
             f.writelines(lines)
 
     def read_measured(self):
-        file_ = '{}/{}'.format(self.hp, 'SELECTOR.IN')
+        file_ = '{}/{}'.format(self.hp, 'Fit.out')
         with open(file_,'r') as f:
             lines = f.readlines()
-        NMat = int(lines[13].split()[0])
 
+        counter = 0
+        for line in lines:
+            if 'Observed Quantity' in line:
+                start = counter
+            if 'Parameter estimation with' in line:
+                end = counter
+            counter += 1
 
+        time = []
+        val = []
+        position = []
+        for i in range(start+3, end-3):
+            line = lines[i].split()
+            time.append(line[1])
+            val.append(line[2])
+            position.append(line[4])
+
+        return time, val, position
 
     def model(self, params):
         """ params ::   thr ths Alfa n Ks l
@@ -55,10 +86,12 @@ class Optim(object):
 
         self.set_params(params)
 
-
-
         #os.system(cmd)
 
+        ss = sumofsquares()
+        return (ss)
+
     def run(self):
-        initparams = self.get_params()
-        self.model(initparams)
+        pass
+        #initparams = self.get_params()
+        #self.model(initparams)
