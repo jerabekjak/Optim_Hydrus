ee216db78f5d1f7c164506c9e2bbf9c3efea0f19
jerabekjak <jerabekjaku@gmail.com>
2021-04-26 10:44:45 +0200

    add Optim class

---
+++ optim.py
@@ -0,0 +1,6 @@
+class Optim(object):
+    
+    def __init__(self,hydro_proj, outdir):
+        pass
+    def run(self):
+        pass
--- optim_hydrus.py
+++ optim_hydrus.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 import os
+import optim
 from parser import read_parser
 
 def main(pars):
-    print (pars)
 
     outdir = '{}.{}'.format(pars.hydrus_project.strip('/'), 'out')
     if not os.path.exists(outdir):
         os.makedirs(outdir)
+
+    Optim = optim.Optim(pars.hydrus_project, outdir)
+    Optim.run()
 
 if __name__ == "__main__":
     main(read_parser())
