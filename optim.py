8bc7c59fb06e06cba215b6d72678543d6567ee1a
jerabekjak <jerabekjaku@gmail.com>
2021-04-26 11:01:31 +0200

    add documentation to model function

--- optim.py
+++ optim.py
@@ -7,7 +7,10 @@ class Optim(object):
         self.outdir = outdir
         self.exe = 'H1D_CALC.EXE'
 
-    def model(self,params):
+    def model(self, params):
+        """ params ::   thr ths Alfa n Ks l
+        in case of 2 soils paras list of two lists
+        """
         cmd = './{} {}'.format(self.exe, self.hp)
         os.system(cmd)
         
