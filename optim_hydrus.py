0cc83ceaadd2d810599ba5543ed9c9abe74168d3
jerabekjak <jerabekjaku@gmail.com>
2021-04-26 10:38:24 +0200

    create output dir

--- optim_hydrus.py
+++ optim_hydrus.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
+import os
 from parser import read_parser
 
 def main(pars):
     print (pars)
+
+    outdir = '{}.{}'.format(pars.hydrus_project.strip('/'), 'out')
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
 if __name__ == "__main__":
     main(read_parser())
